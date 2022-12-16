9957a7f93c9012e3aef9e8bba215cb6dd7531b76
gabihcaldeira <gabihcaldeira@hotmail.com>
2022-12-15 23:44:48 -0300

    Tarefa 3 completed - update all views

--- users/views.py
+++ users/views.py
@@ -4,7 +4,7 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 from .serializers import UserSerializer
 from django.shortcuts import get_object_or_404
 from .permissions import IsAccountOwner
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView
 
 
 class UserView(CreateAPIView):
@@ -12,26 +12,31 @@ class UserView(CreateAPIView):
     serializer_class = UserSerializer
 
 
-class UserDetailView(APIView):
+class UserDetailView(RetrieveUpdateDestroyAPIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAccountOwner]
 
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+"""
     def get(self, request: Request, pk: int) -> Response:
         """
-        Obtençao de usuário
-        """
-        user = get_object_or_404(User, pk=pk)
+# Obtençao de usuário
+"""
+    user = get_object_or_404(User, pk=pk)
 
-        self.check_object_permissions(request, user)
+    self.check_object_permissions(request, user)
 
-        serializer = UserSerializer(user)
+    serializer = UserSerializer(user)
 
-        return Response(serializer.data)
+    return Response(serializer.data)
 
-    def patch(self, request: Request, pk: int) -> Response:
-        """
-        Atualização de usuário
-        """
+def patch(self, request: Request, pk: int) -> Response:
+    """
+# Atualização de usuário
+"""
         user = get_object_or_404(User, pk=pk)
 
         self.check_object_permissions(request, user)
@@ -44,8 +49,8 @@ class UserDetailView(APIView):
 
     def delete(self, request: Request, pk: int) -> Response:
         """
-        Deleçao de usuário
-        """
+# Deleçao de usuário
+"""
         user = get_object_or_404(User, pk=pk)
 
         self.check_object_permissions(request, user)
@@ -53,3 +58,4 @@ class UserDetailView(APIView):
         user.delete()
 
         return Response(status=status.HTTP_204_NO_CONTENT)
+"""
